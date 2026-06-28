0beb1abca159124abeabf1c64f302337e8bc0ff9
Valentin Patti <gonzalezvalentin62c1@gmail.com>
2026-06-28 12:50:47 -0300

    agregue la ruta de proveedor junto con su controller

--- Backend/routes/index.routes.js
+++ Backend/routes/index.routes.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const indexRouter = express.Router();
 const rutaMedicamento = require('./medicamento.routes.js')
+const rutaProveedor = require("./proveedor.routes.js");
 
 indexRouter.use('/medicamentos', rutaMedicamento)
+indexRouter.use("/proveedores", rutaProveedor);
 
 module.exports = indexRouter
