3dc121a9b440d116873f62ea3ef3c59086b7232c
Valentin Patti <gonzalezvalentin62c1@gmail.com>
2026-06-29 05:14:39 -0300

    agregue el dashboard principal

--- Frontend/js/dashboard.js
+++ Frontend/js/dashboard.js
@@ -3,7 +3,8 @@ const botonLogout = document.getElementById("btnLogout")
 botonLogout.addEventListener("click", cerrarSesion)
 
 const rol = localStorage.getItem("rol");
-
+const nombre = localStorage.getItem("nombre")
+const tituloSaludo = document.getElementById("saludoUsuario")
 //oculto opciones para el rol empleado
 if (rol === "empleado") {
 
@@ -11,4 +12,8 @@ if (rol === "empleado") {
     document.getElementById("compras").style.display = "none";
     document.getElementById("proveedores").style.display = "none";
 
+}
+//saludo personalizado
+if (nombre) {
+    tituloSaludo.textContent = `Hola, ${nombre}`
 }
