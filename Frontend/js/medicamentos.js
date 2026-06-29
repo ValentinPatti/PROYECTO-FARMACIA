52c2baef7d96189969f37d80aacc58f6b306f65d
Valentin Patti <gonzalezvalentin62c1@gmail.com>
2026-06-29 03:43:35 -0300

    agregue las paginas de clientes, compras, empleados,medicamentos, proveedores y ventas

--- Frontend/js/medicamentos.js
+++ Frontend/js/medicamentos.js
@@ -1,3 +1,11 @@
 verificarAutenticacion();
 
 verificarRol("administrador", "empleado");
+
+const rol = localStorage.getItem("rol");
+
+if (rol === "empleado") {
+
+    document.getElementById("btnNuevoMedicamento").style.display = "none";
+
+}
